ed85ed222022debeb2829baa0edfc0153463b0ba
harrylaou <harrylaou@gmail.com>
2016-05-04 11:28:46 +0200

    bla

--- src/test/scala/fizzbuzz/BuzzActorSpec.scala
+++ src/test/scala/fizzbuzz/BuzzActorSpec.scala
@@ -2,8 +2,13 @@ package fizzbuzz
 
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import akka.util.Timeout
 import fizzbuzz.FizzBuzzMessages.{Reply, Request}
 import org.scalatest.{Matchers, WordSpecLike}
+
+import scala.util.Success
+import scala.concurrent.duration._
+import akka.pattern.ask
 
 class BuzzActorSpec extends TestKit(ActorSystem("TestSystem"))
   with WordSpecLike
@@ -13,11 +18,27 @@ class BuzzActorSpec extends TestKit(ActorSystem("TestSystem"))
   //TODO: 2. test actor with telling syncronously
   "BuzzActor" should {
     "reply with 'Buzz' if number is divisible by 5" in {
-      ???
+      val actorRef = TestActorRef(new BuzzActor)
+      implicit val timeout = Timeout(5 seconds)
+      val request = Request(10, null, 1)
+
+      val future = actorRef ? request
+      val futureResult = future.value.get
+
+      val expectedReply = Reply(Right("Buzz"), request)
+      futureResult should be(Success(expectedReply))
     }
 
     "reply with original number if it is not divisible by 5" in {
-      ???
+      val actorRef = TestActorRef(new BuzzActor)
+      implicit val timeout = Timeout(5 seconds)
+      val request = Request(9, null, 1)
+
+      val future = actorRef ? request
+      val futureResult = future.value.get
+
+      val expectedReply = Reply(Left(9), request)
+      futureResult should be(Success(expectedReply))
     }
   }
 }
--- src/test/scala/fizzbuzz/CombinatorActorSpec.scala
+++ src/test/scala/fizzbuzz/CombinatorActorSpec.scala
@@ -1,8 +1,15 @@
 package fizzbuzz
 
 import akka.actor.ActorSystem
-import akka.testkit.TestKit
+import akka.testkit.{TestActorRef, TestKit}
+import akka.util.Timeout
+import fizzbuzz.FizzBuzzMessages.{Reply, Request}
 import org.scalatest.{Matchers, WordSpecLike}
+import akka.pattern.ask
+
+import scala.concurrent.Future
+import scala.util.Success
+import scala.concurrent.duration._
 
 class CombinatorActorSpec extends TestKit(ActorSystem("TestSystem"))
   with WordSpecLike
@@ -12,7 +19,17 @@ class CombinatorActorSpec extends TestKit(ActorSystem("TestSystem"))
   //TODO: 3. Test combinator actor synchronously
   "Combinator actor" should {
     "store the pending requests and increase sequence number" in {
-      ???
+      val actorRef = TestActorRef(new CombinatorActor)
+      implicit val timeout = Timeout(5 seconds)
+      val request = Request(9, null, 1)
+      val expectedReply: Reply = Reply(Right("Fizz"), request)
+
+//      actorRef.underlyingActor.pending =
+      val future = actorRef ! 9
+      actorRef.underlyingActor.pending.get(request).get === expectedReply
+      actorRef.underlyingActor.seqNum === 1
+
+
     }
 
     "remove processed requests from pending" in {
